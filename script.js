9aeb084698e478abaffa52fce9412103728fcca4
HNAnh02 <hna281202@gmail.com>
2025-02-23 19:32:48 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const messages = [
+    "thật á",
+    "em chắc chứ",
+    "em nghiêm túc chứ",
+    "thui mừ , hiccc",
+    "suy nghĩ lại đi bé, hicc",
+    "anh sẽ buồn lắm á (╥﹏╥)",
+    "thui đồng ý đi mừ",
+    "chọn lại đi bé iu ưi",
+    "thôi anh sẽ không làm phiền em nữa vậy (╥ᆺ╥;)",
+    "đùa chút thui, bé đồng ý làm nngười yêu anh nhé (๑>◡<๑)"
+];
+
+let messageIndex = 0;
+
+function handleNoClick() {
+    const noButton = document.querySelector('.no-button');
+    const yesButton = document.querySelector('.yes-button');
+    noButton.textContent = messages[messageIndex];
+    messageIndex = (messageIndex + 1) % messages.length;
+    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+    yesButton.style.fontSize = `${currentSize * 1.5}px`;
+}
+
+function handleYesClick() {
+    window.location.href = "yes_page.html";
+}
